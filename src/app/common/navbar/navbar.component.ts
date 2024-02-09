9c2a6e6b75231f3fd912b113d397e16c3806099b
yokoyama takuji <gato.02010417@gmail.com>
2024-02-09 10:34:13 +0900

    navbar追加

---
+++ src/app/common/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+}
